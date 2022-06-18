b0107596c90820fb4ac7fe5a616094827baf9c20
r0fus0d <d2hwakh7bs5e@protonmail.com>
2022-06-19 04:44:11 +0800

    update 2022年 6月19日 星期日 04时44分11秒 CST

---
+++ go.mod
@@ -0,0 +1,13 @@
+module mapcidr2
+
+go 1.18
+
+require (
+	github.com/projectdiscovery/mapcidr v1.0.0
+	github.com/thinkeridea/go-extend v1.3.2
+)
+
+require (
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/projectdiscovery/blackrock v0.0.0-20210415162320-b38689ae3a2e // indirect
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"github.com/projectdiscovery/mapcidr"
+	"github.com/thinkeridea/go-extend/exnet"
+	"net"
+	"os"
+	"strings"
+)
+
+var filename = flag.String("in", "", "输入文件名")
+var cidrSlice []string
+var iprangeSlice []string
+var wrongSlice []string
+var ipv4_left = ""
+var ipv4_right = ""
+var numSlice []uint
+
+func main() {
+
+	flag.Parse()
+	if *filename == "" {
+		*filename = "文件名不可为空!!!"
+	} else {
+		todo()
+	}
+
+}
+
+func todo() {
+
+	file, err := os.OpenFile(*filename, os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println("打开文件失败!", err)
+		return
+	}
+	defer file.Close()
+
+	buf1 := bufio.NewScanner(file)
+	for buf1.Scan() {
+		// cird
+		check1 := strings.Contains(buf1.Text(), "/")
+
+		// ip-range
+		check2 := strings.Contains(buf1.Text(), "-")
+
+		if check1 {
+			//fmt.Println(buf1.Text(), "cird")
+			cidrSlice = append(cidrSlice, buf1.Text())
+		} else if check2 {
+			//fmt.Println(buf1.Text(), "ip-range")
+			iprangeSlice = append(iprangeSlice, buf1.Text())
+		} else {
+			// 非预期格式
+			//fmt.Println(buf1.Text(), "wrong")
+			wrongSlice = append(wrongSlice, buf1.Text())
+		}
+	}
+
+	// 处理 ip-range 队列
+	iprange()
+
+	// 处理 cidr 队列
+	cidr()
+
+	// 处理非预期格式
+	fmt.Println("-----非预期格式-----")
+	wrong()
+
+}
+
+func iprange() {
+
+	for _, v := range iprangeSlice {
+		arr := strings.Split(v, "-")
+		if len(arr) == 2 {
+			ipv4_left = arr[0]
+			ipv4_right = arr[1]
+			// 左边地址
+			addressRight := net.ParseIP(ipv4_left)
+			// 右边地址
+			addressRight2 := net.ParseIP(ipv4_right)
+			if addressRight == nil {
+				// 格式非预期
+				wrongSlice = append(wrongSlice, v)
+			} else if addressRight2 == nil {
+				// 格式非预期
+				wrongSlice = append(wrongSlice, v)
+			} else {
+				// 转10进制
+				num1, _ := exnet.IPString2Long(ipv4_left)
+				num2, _ := exnet.IPString2Long(ipv4_right)
+				if num1 > num2 {
+					// 格式非预期
+					wrongSlice = append(wrongSlice, v)
+				} else {
+					// 循环自增
+					for {
+						// 添加进切片
+						numSlice = append(numSlice, num1)
+						if num1 == num2 {
+							break
+						}
+						num1 += 1
+					}
+				}
+			}
+		} else {
+			// 格式非预期
+			wrongSlice = append(wrongSlice, v)
+		}
+	}
+
+	for _, v := range numSlice {
+		s, _ := exnet.Long2IPString(v)
+		fmt.Println(s)
+	}
+
+}
+
+func cidr() {
+
+	for _, v := range cidrSlice {
+		ips, _ := mapcidr.IPAddresses(v)
+		for _, ip := range ips {
+			fmt.Println(ip)
+		}
+	}
+
+}
+
+func wrong() {
+
+	for _, v := range wrongSlice {
+		fmt.Println(v)
+	}
+
+}
